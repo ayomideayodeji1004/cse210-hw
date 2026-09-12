4428afe8a9b239a047004eb7fbff828578a85d6a
ayomideayodeji <ayomideayodeji1004@gmail.com>
2026-09-12 14:44:53 -0700

    save

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,12 @@ using System;
 
 class Program
 {
+   
+   
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("welcome to the program.");
+         
+        
     }
 }
